d729f882829a2cff4564a63a65dd1b503fc2badf
salihkaankoc <72124091+salihkaankoc@users.noreply.github.com>
2022-09-06 13:55:22 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,23 @@
+import 'package:denemeproje/models/cart.dart';
+import 'package:denemeproje/screens/homepage.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(ChangeNotifierProvider(
+    create: (context) => CartModel(),
+    child: const MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
